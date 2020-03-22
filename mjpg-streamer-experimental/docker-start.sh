b242f790ff5e2976b4acee7a5bcedf226cafcd48
GibsDev <notcurtisg@gmail.com>
2020-03-22 01:01:47 -0400

    changed resolution

--- mjpg-streamer-experimental/docker-start.sh
+++ mjpg-streamer-experimental/docker-start.sh
@@ -3,4 +3,4 @@ set -e
 
 export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
 #./mjpg_streamer -o "$1" -i "$2"
-./mjpg_streamer -i "./input_uvc.so -n -f 30 -r 640x480 -d /dev/video0"  -o "./output_http.so -w ./www"
+./mjpg_streamer -i "./input_uvc.so -n -f 30 -r 1280x720 -d /dev/video0"  -o "./output_http.so -w ./www"
