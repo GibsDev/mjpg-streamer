9ea98f2c97745fbc23df021cb74bbdc14c8d711a
GibsDev <notcurtisg@gmail.com>
2020-03-22 00:54:06 -0400

    modified docker-start for custom webcam params

--- mjpg-streamer-experimental/docker-start.sh
+++ mjpg-streamer-experimental/docker-start.sh
@@ -2,4 +2,5 @@
 set -e
 
 export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
-./mjpg_streamer -o "$1" -i "$2"
+#./mjpg_streamer -o "$1" -i "$2"
+./mjpg_streamer -i "./input_uvc.so -n -f 30 -r 640x480 -d /dev/video0"  -o "./output_http.so -w ./www"
